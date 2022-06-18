63aa81e1113dc2a553db5a867cef10be4efb8f85
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-18 02:38:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
 
+    }
+
+}
+return true;
+}
 /* 
   Add your pseudocode here
 */
@@ -15,6 +24,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
